73c1519eb711f5fafb3f08f10e91864a1d561a2a
CRT_HAO <m831718@gmail.com>
2023-03-28 18:12:55 +0800

    Add Week 5 Framework

---
+++ Part/Week05/Week05_Framework/Week05/Trigger.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "Position.h"
+#include "Hero.h"
+
+
+//************************************************************
+// Trigger Class
+//************************************************************
+class Trigger {
+	// Implement Trigger Class
+	/*Please implement your code here*/
+
+	/************************************************************************/
+};
+
