871477fe31041b76528891b6862f05b9a627267d
CRT_HAO <m831718@gmail.com>
2023-04-25 21:20:52 +0800

    Complete Week 8 Part

--- Part/Week08/Week08_Framework/Week08/main.h
+++ Part/Week08/Week08_Framework/Week08/main.h
@@ -1,15 +1,28 @@
-#include <iostream>
-#include <math.h>		// pow() and sqrt()
-#include <ctime>		// for time loop function
-#include <conio.h>		// _getch() <= to read input without enter "ENTER" key
-#include <algorithm>	// for max and min function
+/**
+ *  File: main.h
+ *  Author: 張皓鈞 (B11030202@mail.ntust.edu.tw)
+ *  Author: 鄭健廷 (B11130225@mail.ntust.edu.tw)
+ *  Create Date: 2023/04/25 21:08:31
+ *  Editor: 張皓鈞(HAO) m831718@gmail.com
+ *  Update Date: 2023/04/25 21:12:37
+ *  Description: 基本輸入方向移動功能，w s a d 移動腳色上下左右，空白改變腳色站立之地板字元，
+ *               到T上可以增加經驗，ESC是離開畫面。同時更新圖版上的資訊。
+ */
+
 #include "Position.h"
 #include "Trigger.h"
+
+#include <algorithm> // for max and min function
+#include <conio.h>   // _getch() <= to read input without enter "ENTER" key
+#include <ctime>     // for time loop function
+#include <iostream>
+#include <math.h> // pow() and sqrt()
+
 // Check whether the location is a valid one i.e. not a wall
-bool isPositionValid(Position& loc);
+bool isPositionValid(Position &loc);
+
 // clip algroithm
 float clip(float n, float minimun, float maximum);
 
-// Check whether two position is close enough�A�Q�� call-by-referce �N�p��o�쪺��V�^��
-bool canSee(Position cPos, Position hPos, Position& pos2);
-
+// Check whether two position is close enough，利用 call-by-referce 將計算得到的方向回傳
+bool canSee(Position cPos, Position hPos, Position &pos2);
